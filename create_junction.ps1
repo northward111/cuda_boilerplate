aeff6b82736a4fb33051e5d4f21c48c454b103b5
jiangzijie <jzjwhu@qq.com>
2021-08-27 18:57:42 +0800

    update path

--- create_junction.ps1
+++ create_junction.ps1
@@ -1,5 +1,5 @@
 New-Item -ItemType Junction `
-		 -Path D:\src\cuda_test\third_party_win64 `
+		 -Path D:\src\cuda_boilerplate\third_party_win64 `
 		 -Name eigen-3.3.7 `
 		 -Target D:\cpp_libs\third_party_win64\eigen-3.3.7
 		 
